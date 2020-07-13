d4662e35d3758bc9476432013182d333fef52ad8
astapenko <astapenko>
2020-07-13 17:42:13 +0300

    added detekt and updated pre commit hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
     id("org.jlleitschuh.gradle.ktlint") version "6.1.0"
+    id("io.gitlab.arturbosch.detekt") version "1.10.0"
 }
 
 buildscript {
 
     repositories {
         google()
-        jcenter()
+        jcenter() {
+            content {
+                includeGroup("org.jetbrains.kotlinx")
+            }
+        }
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
